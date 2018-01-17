57765bcfc890fbdafb9faa61ac140ff7c806458f
EricV04 <35345906+EricV04@users.noreply.github.com>
2018-01-17 15:29:56 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 console.log("Up and running!"); 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds"
+}
+];
 
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
-}
 
 }
 var flipCard = function (cardId) {
@@ -19,12 +35,14 @@ var flipCard = function (cardId) {
 else {
 	alert("Sorry, try again.");
 }
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+cardsInPlay.push(cards[cardId].rank);
 
 checkForMatch();
 }
 
 flipCard(0);
-flipCard(2);
+flipCard(3);
 
