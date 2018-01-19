79bd149b591173390d2f8db2414ae07e54d0fc7d
EricV04 <35345906+EricV04@users.noreply.github.com>
2018-01-19 09:45:24 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,47 +2,62 @@ console.log("Up and running!");
 
 var cards = [
 {
-rank: 'queen',
-suit: 'hearts',
+rank: "queen",
+suit: "hearts",
 cardImage: "images/queen-of-hearts.png"
 },
 {
-rank: 'queen',
-suit: 'diamonds',
+rank: "queen",
+suit: "diamonds",
 cardImage: "images/queen-of-diamonds.png"
 },
 {
-rank: 'king',
-suit: 'hearts',
-cardImage: "images/king-of-hearts"
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
 },
 {
-rank: 'king',
-suit: 'diamonds',
-cardImage: "images/king-of-diamonds"
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
 }
 ];
 
 var cardsInPlay = [];
 
-var checkForMatch = function () {
+checkForMatch = function () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+};
 
-}
-var flipCard = function (cardId) {
-	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.");
-}
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-cardsInPlay.push(cards[cardId].rank);
+};
 
-checkForMatch();
-}
+flipCard = function () {
+	var cardId = this.getAttribute('data-id')
+	// console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	// console.log(cards[cardId].suit);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
 
-flipCard(0);
-flipCard(3);
+console.log(cardsInPlay);
+
+createBoard = function() {
+  const cardTable = document.getElementById('game-board');
+  for (var i = 0; i < cards.length; i++) {
+  var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    cardTable.appendChild(cardElement);
+
+  }
+};
+
+createBoard();
 
